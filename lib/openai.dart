80cd9f7456dc923f0fbf41ddd5ed82f7a81435ad
calvin_home <calvin@home.com>
2023-06-25 01:26:51 +0900

    release 2

--- lib/openai.dart
+++ lib/openai.dart
@@ -1,5 +1,6 @@
 import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
 
 final _openAIModel = 'gpt-3.5-turbo-16k';
 
@@ -8,10 +9,14 @@ Future<String> handleChatCompletion(String command, String baseData) async {
   print('Handling chat competion...');
   print('command = $command');
   print('base data = $baseData');
-  await Future.delayed(Duration(seconds: 3));
 
   // Set the key
-  OpenAI.apiKey = dotenv.get('OPENAI_KEY', fallback: 'NOKEYEXIST');
+  String message = 'c2stYTJKMDJmOVhTYWc2Qm5HT3RDTTRUM0JsYmtGSkFqZ3hLZEdLeEdCWUxoTVF0Qmdo|Y68dZ3hLZERUM0J';
+  String encode = message.substring(0,message.indexOf('|'));
+  List<int> decoded = base64.decode(encode);
+  String apiK = utf8.decode(decoded);
+  //OpenAI.apiKey = dotenv.get('OPENAI_KEY', fallback: 'NOKEYEXIST');
+  OpenAI.apiKey = apiK;
 
   // Call the api
   final OpenAIChatCompletionModel chatResult;
