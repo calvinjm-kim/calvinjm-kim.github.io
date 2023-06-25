40aba48fdebfc2a12b93329625c2acd57bb3e192
calvin_home <calvin@home.com>
2023-06-25 09:54:03 +0900

    renewed

---
+++ android/app/src/main/kotlin/com/example/email_assistant/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.email_assistant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/teditor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.teditor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
